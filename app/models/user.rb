3237cc1709557b8512aa5387581844a9f9474084
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-07-11 16:02:49 -0500

    Comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+  # Relations
+  
+  # Validations
+  
+  # Scopes
+
+  # Devise
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Methods
 end
