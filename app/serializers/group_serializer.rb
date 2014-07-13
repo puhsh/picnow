9f90909e8a8bc3702ba806f6def32d00672fb62e
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-07-13 14:43:43 -0500

    N+1 fixes

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -2,8 +2,4 @@ class GroupSerializer < ActiveModel::Serializer
   attributes :id, :last_photo_sent_at, :created_at, :updated_at
   has_many :users
   has_many :photos
-
-  def photos
-    object.photos.order('created_at desc').limit(20)
-  end
 end
