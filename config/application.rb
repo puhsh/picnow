dc97e1a37acea70127834259569dcae5ab101677
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-07-15 22:42:33 -0500

    Hide da logs

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module PicNow
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.filter_parameters += [:password, :password_confirmation, :access_token]
   end
 end
