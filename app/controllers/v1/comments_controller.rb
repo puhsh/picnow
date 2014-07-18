13020a2cb09b82e4e3e8756f31982492d89919a9
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-07-18 10:32:46 -0500

    Comment fix

--- app/controllers/v1/comments_controller.rb
+++ app/controllers/v1/comments_controller.rb
@@ -2,7 +2,11 @@ class V1::CommentsController < V1::ApiController
   before_filter :verify_access_token
 
   def create
+    @user = User.find(params[:user_id])
+    @group = Group.find(params[:group_id])
     @comment = Comment.new(params[:comment])
+    @comment.user = @user
+    @comment.group = @group
     if @comment.save
       render json: @comment
     else
