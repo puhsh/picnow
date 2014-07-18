c5a6b773b0e77d14a9b385d9f5eebee8debdc91f
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-07-18 08:39:46 -0500

    Parameter cleanup

--- app/controllers/v1/auth_controller.rb
+++ app/controllers/v1/auth_controller.rb
@@ -2,8 +2,11 @@ class V1::AuthController < V1::ApiController
   after_filter :set_csrf_header, only: [:create]
 
   def create
-    resource = User.find_by_email(params[:email]) || User.find_by_username(params[:username])
-    if resource && resource.valid_password?(params[:password])
+    unprocessable_entity! unless params[:user]
+
+    resource = User.find_by_email(params[:user][:email]) || User.find_by_username(params[:user][:username])
+
+    if resource && resource.valid_password?(params[:user][:password])
       sign_in 'user', resource
       render json: resource.as_json.merge({access_token: resource.access_token})
     else
