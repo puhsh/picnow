254913bfa02e79240fe4e3d9dde6863a87ffa93b
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-09-07 08:58:56 -0500

    Auth fixes. Generate an access token when you seed the DB

--- app/controllers/v1/auth_controller.rb
+++ app/controllers/v1/auth_controller.rb
@@ -5,12 +5,13 @@ class V1::AuthController < V1::ApiController
   def create
     unprocessable_entity! unless params[:user]
 
-    resource = User.find_by_email(params[:user][:email]) || User.find_by_username(params[:user][:username])
+    resource = User.find_by_username(params[:user][:username])
 
     if resource && resource.valid_password?(params[:user][:password])
-      warden.authenticate! scope: 'user'
+      # warden.authenticate! scope: 'user'
       sign_in 'user', resource
-      render json: resource.as_json.merge({access_token: resource.access_token})
+      user = UserSerializer.new(resource)
+      render json: user.as_json.merge({access_token: resource.access_token})
     else
       warden.custom_failure!
       unauthorized!
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 User.create({username: 'testuser', email: 'test@test.local', password: '12345678', phone_number: '555-555-5555', date_of_birth: DateTime.now - 20.years})
+User.first.generate_access_token!
 Group.create(name: 'My Group')
 GroupUser.create(user: User.first, group: Group.first)
