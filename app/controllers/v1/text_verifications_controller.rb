5dd42017d69163869b5d16866a2a7b978b2aa003
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-09-11 08:58:17 -0500

    User ID not Group ID

--- app/controllers/v1/text_verifications_controller.rb
+++ app/controllers/v1/text_verifications_controller.rb
@@ -2,7 +2,7 @@ class V1::TextVerificationsController < V1::ApiController
   before_filter :verify_access_token
 
   def create
-    @user = User.find(params[:group_id])
+    @user = User.find(params[:user_id])
     if @user
       @user.generate_text_verification!
       respond_with @user
@@ -12,7 +12,7 @@ class V1::TextVerificationsController < V1::ApiController
   end
 
   def update
-    @user = User.find(params[:group_id])
+    @user = User.find(params[:user_id])
     if @user && @user.text_verification
       @user.text_verification.save
       respond_with @user
