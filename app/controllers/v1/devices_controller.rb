96894f8542677b2bb9c2aab5b622cddda410720b
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-09-16 17:23:37 -0500

    Strong params for Devices

--- app/controllers/v1/devices_controller.rb
+++ app/controllers/v1/devices_controller.rb
@@ -2,11 +2,17 @@ class V1::DevicesController < V1::ApiController
   before_filter :verify_access_token
 
   def create
-    @device = Device.new(params[:device])
+    @device = Device.new(device_params)
     if @device.save
       render json: @device
     else
       unprocessable_entity!
     end
+  end
+
+  private
+
+  def device_params
+    params.require(:device).permit(:token, :brand)
   end
 end
