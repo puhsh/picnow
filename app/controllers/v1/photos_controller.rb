44d359699e758a523c1ad58d750794d150d1ac16
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-10-17 06:40:05 -0500

    Probably should assign a photo to a group.

--- app/controllers/v1/photos_controller.rb
+++ app/controllers/v1/photos_controller.rb
@@ -9,8 +9,10 @@ class V1::PhotosController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
+    @group = Group.find(params[:group_id])
     @photo = Photo.new(params[:photo])
     @photo.user = @user
+    @photo.group = @group
     if @photo.save
       render json: @photo
     else
