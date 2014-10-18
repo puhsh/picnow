00923620d2ff32ae07eaccf40797f36769210d6e
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-10-18 10:04:08 -0500

    Whitelisted

--- app/controllers/v1/devices_controller.rb
+++ app/controllers/v1/devices_controller.rb
@@ -2,7 +2,7 @@ class V1::DevicesController < V1::ApiController
   before_filter :verify_access_token
 
   def create
-    @device = Device.new(device_params, user_id: params[:user_id])
+    @device = Device.new(device_params)
     if @device.save
       render json: @device
     else
