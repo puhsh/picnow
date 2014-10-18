218763b7c7f1256d7e4aaddd9ce2fbc58a60154a
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-10-18 08:52:29 -0500

    Sorting

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -7,7 +7,7 @@ class V1::UsersController < V1::ApiController
   def index
     if params[:group_id]
       @group = Group.includes(:users).find(params[:group_id])
-      @users = @group.users
+      @users = @group.users.order(created_at: :asc)
     end
     render json: @users
   end
