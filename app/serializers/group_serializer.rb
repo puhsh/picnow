689fffd4bde56d0cb0df15d08849cfe3075a2ed8
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-10-25 13:43:53 -0500

    Stubbing out notifications

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,4 +1,8 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :name, :last_photo_sent_at, :created_at, :updated_at
+  attributes :id, :name, :last_photo_sent_at, :created_at, :updated_at, :unread_notifications_count
   has_many :photos
+
+  def unread_notifications_count
+    0
+  end
 end
