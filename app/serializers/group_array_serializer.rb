49a7e28bd5747733fe9e22dc2a111ade32d577f5
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-10-25 14:52:24 -0500

    API updates

--- app/serializers/group_array_serializer.rb
+++ app/serializers/group_array_serializer.rb
@@ -1,3 +1,7 @@
 class GroupArraySerializer < ActiveModel::Serializer
-  attributes :id, :name, :last_photo_sent_at, :created_at, :updated_at
+ attributes :id, :name, :last_photo_sent_at, :created_at, :updated_at, :unread_notifications_count
+
+  def unread_notifications_count
+    object.notifications.where(read: false).count
+  end
 end
--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,8 +1,3 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :name, :last_photo_sent_at, :created_at, :updated_at, :unread_notifications_count
-  has_many :photos
-
-  def unread_notifications_count
-    object.notifications.where(read: false).count
-  end
+  attributes :id, :name, :last_photo_sent_at, :created_at, :updated_at  
 end
