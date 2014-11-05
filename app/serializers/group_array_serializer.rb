1db2ad96610d1e50d314982853926bf12c1c1309
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-11-05 07:28:24 -0600

    Serializer fix

--- app/serializers/group_array_serializer.rb
+++ app/serializers/group_array_serializer.rb
@@ -1,5 +1,5 @@
 class GroupArraySerializer < ActiveModel::Serializer
- attributes :id, :name, :last_photo_sent_at, :created_at, :updated_at, :unread_notifications_count
+ attributes :id, :name, :last_photo_sent_at, :created_at, :updated_at, :unread_notifications_count, :admin_id
 
   def unread_notifications_count
     object.notifications.where(read: false).count
--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,3 +1,3 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :name, :last_photo_sent_at, :created_at, :updated_at  
+  attributes :id, :name, :last_photo_sent_at, :created_at, :updated_at, :admin_id
 end
