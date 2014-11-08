7f7bbeee156009eec9931f2b721bf2a4d6d4bf8c
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-11-07 18:37:10 -0600

    Send an APN

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,7 @@ class Comment < ActiveRecord::Base
   belongs_to :group
 
   # Callbacks
+  after_commit :notify_group_users, on: :create
   
   # Validations
   validates :content, presence: true, length: { maximum: 500 }
@@ -11,4 +12,14 @@ class Comment < ActiveRecord::Base
   # Scopes
 
   # Methods
+
+  protected
+
+  def notify_group_users
+    group_users = GroupUser.includes(:user).where(group_id: self.group_id).where.not(user_id: self.user_id)
+    group_users.each do |group_user|
+      Notification.create(user_id: group_user.user_id, group_id: group_user.group_id)
+      group_user.user.devices.each { |x| x.fire_notification!("New comment from #{self.user.username}", :picnow_comment) }
+    end
+  end
 end
