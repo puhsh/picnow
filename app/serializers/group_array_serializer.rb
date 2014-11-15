f34790cbe6e1fbabe97569d51c407063889ee66a
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-11-15 08:09:24 -0600

    Whoops...

--- app/serializers/group_array_serializer.rb
+++ app/serializers/group_array_serializer.rb
@@ -2,6 +2,6 @@ class GroupArraySerializer < ActiveModel::Serializer
  attributes :id, :name, :last_photo_sent_at, :created_at, :updated_at, :unread_notifications_count, :admin_id
 
   def unread_notifications_count
-    object.notifications.where(read: false).count
+    object.notifications.where(read: false, user_id: current_user.id).count
   end
 end
