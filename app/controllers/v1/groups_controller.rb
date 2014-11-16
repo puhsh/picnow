7085e75fd59be0f201eb006eb405f03280b3a13c
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-11-16 08:18:38 -0600

    Let's try a limit

--- app/controllers/v1/groups_controller.rb
+++ app/controllers/v1/groups_controller.rb
@@ -26,7 +26,7 @@ class V1::GroupsController < V1::ApiController
 
   def activity
     @group = Group.find(params[:id])
-    @activity = @group.events.map(&:payload)
+    @activity = @group.events.limit(100).map(&:payload)
     render_paginated @activity
   end
   
