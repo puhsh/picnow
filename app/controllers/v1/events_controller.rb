4435ff046cfddd1daf229c41cf0b19147951b0fe
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-11-16 11:57:00 -0600

    Bump it up

--- app/controllers/v1/events_controller.rb
+++ app/controllers/v1/events_controller.rb
@@ -1,7 +1,7 @@
 class V1::EventsController < V1::ApiController
   def index
     @group = Group.find(params[:group_id])
-    @events = Event.includes(:user).where(group_id: @group.id).limit(30).order(created_at: :desc).reverse
+    @events = Event.includes(:user).where(group_id: @group.id).limit(50).order(created_at: :desc).reverse
     render_paginated @events
   end
 end
