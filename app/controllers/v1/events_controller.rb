0bdf6e33dadc1bd603d313304a318fb53979d088
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-11-16 08:57:43 -0600

    Damn API

--- app/controllers/v1/events_controller.rb
+++ app/controllers/v1/events_controller.rb
@@ -1,7 +1,7 @@
 class V1::EventsController < V1::ApiController
   def index
     @group = Group.find(params[:group_id])
-    @events = Event.includes(:user).where(group_id: @group.id)
+    @events = Event.includes(:user).where(group_id: @group.id).limit(100).order(created_at: :desc).reverse
     render_paginated @events
   end
 end
