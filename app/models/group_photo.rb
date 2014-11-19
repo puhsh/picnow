fa027c49efa3021be3b39f790a76ebb0e05cfbaf
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-11-19 10:40:06 -0600

    Can't send photos to yourself.

--- app/models/group_photo.rb
+++ app/models/group_photo.rb
@@ -7,10 +7,22 @@ class GroupPhoto < ActiveRecord::Base
   belongs_to :user
 
   # Callbacks
+  before_save :zero_out_points
   
   # Validations
   
   # Scopes
 
   # Methods
+
+  protected
+
+  # Protected: Zero outs points if there is no one in the group
+  #
+  # Returns a 0 if the value is adjusted
+  def zero_out_points
+    unless GroupUser.where(group_id: self.group_id).exists?
+      self.point_value = 0
+    end
+  end
 end
