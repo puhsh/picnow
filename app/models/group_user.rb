d18e2961e7e9d3f578bf8b6c0cba1a4f075b5f9e
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-11-20 11:38:36 -0600

    Notify when someone is added to a group

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -4,6 +4,7 @@ class GroupUser < ActiveRecord::Base
   belongs_to :user
 
   # Callbacks
+  after_commit :notify_user, on: :create
   
   # Validations
   validates :user, presence: true
@@ -13,4 +14,17 @@ class GroupUser < ActiveRecord::Base
   scope :not_deleted, -> { where(deleted_at: nil) }
 
   # Methods
+
+  protected
+
+  # Protected: Notifies the user that they were added to a group
+  #
+  # Returns nothing
+  def notify_user
+    group = self.group
+    admin = self.group.admin
+    if group && admin
+      self.user.devices.each { |x| x.fire_notification!("#{admin.username} added you to the group #{group.name}", :added_to_group) }
+    end
+  end
 end
