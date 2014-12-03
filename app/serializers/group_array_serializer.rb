f73d7724e2d3389212e4ef6a0e9f0c0ddd0b91dc
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-12-03 09:00:38 -0600

    Nil check

--- app/serializers/group_array_serializer.rb
+++ app/serializers/group_array_serializer.rb
@@ -2,6 +2,10 @@ class GroupArraySerializer < ActiveModel::Serializer
  attributes :id, :name, :last_photo_sent_at, :created_at, :updated_at, :unread_notifications_count, :admin_id
 
   def unread_notifications_count
-    object.notifications.where(read: false, user_id: @scope.id).count
+    if @scope
+      object.notifications.where(read: false, user_id: @scope.id).count  
+    else
+      0
+    end
   end
 end
