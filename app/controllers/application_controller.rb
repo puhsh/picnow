0d4ac9929a28fa3fd5a1263856b21562bdf005f6
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-12-04 10:27:56 -0600

    NVM

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   respond_to :html
-
-  before_filter :ignore_api_browser_request
-
-
-  protected
-
-  def ignore_api_browser_request
-    if request.subdomain == 'api' 
-      redirect_to root_url, subdomain: 'www'
-    end
-  end
 end
