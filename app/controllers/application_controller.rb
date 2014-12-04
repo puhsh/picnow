aac5aef7f94bd923f3c1199808f212de5d335c1c
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-12-04 10:24:48 -0600

    Don't need people hitting the API in the browser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   respond_to :html
+
+  before_filter :ignore_api_browser_request
+
+
+  protected
+
+  def ignore_api_browser_request
+    if request.subdomain == 'api' 
+      render status: 404
+    end
+  end
 end
