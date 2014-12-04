d796ba360e4cfc315c8901818ae8bdce794180a5
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-12-04 14:10:26 -0600

    Serialize

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,3 +1,3 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :name, :last_photo_sent_at, :created_at, :updated_at, :admin_id
+  attributes :id, :name, :last_photo_sent_at, :created_at, :updated_at, :admin_id, :points_enabled, :group_type
 end
