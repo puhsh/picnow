640991c375d2eb0c00eb5260b6ecc60bb6626808
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-12-09 13:51:05 -0600

    Memcache bump

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, 'picnow-production.fvex8b.cfg.use1.cache.amazonaws.com:11211'
+  config.cache_store = :dalli_store, 'picnow.fvex8b.cfg.use1.cache.amazonaws.com:11211'
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
