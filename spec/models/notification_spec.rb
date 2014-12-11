422ef13824fe1619b2f78fae59104bce3857d11b
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2014-12-10 23:00:30 -0600

    Initial notification specs

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Notification do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:group) }
+  it { should belong_to(:trigger) }
 end
