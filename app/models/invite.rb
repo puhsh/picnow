d90708e528c8bd4b874acc4ac498945b25d3ddbb
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-01-02 15:05:56 -0600

    Invite spec

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,7 +1,7 @@
 class Invite < ActiveRecord::Base
   # Relations
   belongs_to :user
-  belongs_to :joined_user
+  belongs_to :joined_user, class_name: 'User'
   belongs_to :group
 
   # Callbacks
--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Invite do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:joined_user) }
+  it { should belong_to(:group) }
 end
