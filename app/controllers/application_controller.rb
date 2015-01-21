d2ba9191d2c971a91d71ce5738af16c47b1c35f0
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-01-21 08:08:38 -0600

    Support mobile safari...technically.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   respond_to :html
 
+  def ios_safari_request?
+    request.user_agent && request.user_agent.match(/Safari/) && (request.user_agent.match(/Mozilla\/.*iPhone/) || request.user_agent.match(/Mozilla\/.*iPad/))
+  end
+  helper_method :ios_safari_request?
+
   def ios_native_request?
     request.user_agent && request.user_agent.match(/PicNow-iOS/)
   end
