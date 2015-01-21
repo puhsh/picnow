142bbe1bb69bd9f4ec49b22aca029f23086033e6
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-01-21 08:01:10 -0600

    Need to send people to the right store

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   respond_to :html
+
+  def ios_native_request?
+    request.user_agent && request.user_agent.match(/PicNow-iOS/)
+  end
+  helper_method :ios_native_request?
+
+  def android_native_request?
+    request.user_agent && request.user_agent.match(/PicNow-Android/)
+  end
+  helper_method :android_native_request
 end
