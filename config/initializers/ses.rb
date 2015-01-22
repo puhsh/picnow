0f8785ad7558956f288a4ed221f4f68085c203a9
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-01-21 22:41:07 -0600

    File name

--- config/initializers/ses.rb
+++
@@ -1,3 +0,0 @@
-ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  access_key_id: Rails.application.secrets[:ses]['access_key_id'],
-  secret_access_key: Rails.application.secrets[:ses]['secret_access_key']
