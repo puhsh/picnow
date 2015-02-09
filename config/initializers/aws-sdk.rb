8f695d178b119e224c2be0bc7d0a83cdc2149fae
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-02-09 15:12:13 -0600

    Use secrets

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -6,6 +6,6 @@ AWS.config({
 
 # SES
 AWS::Rails.add_action_mailer_delivery_method(:ses, {
-  access_key_id: 'AKIAIIZUHZAMMOZFHLGA',
-  access_key_secret: 'Aljf7UCs4An1JH8zzgsGfJfCtdBckUH2rzSpF9wyYGzB'
+  access_key_id: Rails.application.secrets[:ses]['access_key_id'],
+  access_key_secret: Rails.application.secrets[:ses]['access_key_secret']
 })
