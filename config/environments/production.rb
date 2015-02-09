84ca023a7182f4e7220e268ca5b1f05efea1678c
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-02-09 15:15:45 -0600

    Almost

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'email-smtp.us-east-1.amazonaws.com',
     port: '25'
+    authentication: :login
   }
   config.action_mailer.default_url_options = {
     host: 'my.picnow.rocks'
