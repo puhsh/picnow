5baff04a4e3544a33b2f15e6fc2b2d6c59fd7652
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-02-09 15:09:06 -0600

    Generic config

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -1,10 +1,11 @@
-# S3
-AWS::S3.new(
-  access_key_id: Rails.application.secrets[:s3]['access_key_id'],
-  secret_access_key: Rails.application.secrets[:s3]['secret_access_key'])
+# General Config
+AWS.config({
+  access_key_id: '',
+  access_key_secret: ''
+})
 
-
+# SES
 AWS::Rails.add_action_mailer_delivery_method(:ses, {
-    access_key_id: 'AKIAIIZUHZAMMOZFHLGA',
-    access_key_secret: 'Aljf7UCs4An1JH8zzgsGfJfCtdBckUH2rzSpF9wyYGzB'
-  })
+  access_key_id: 'AKIAIIZUHZAMMOZFHLGA',
+  access_key_secret: 'Aljf7UCs4An1JH8zzgsGfJfCtdBckUH2rzSpF9wyYGzB'
+})
