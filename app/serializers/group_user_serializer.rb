109d788257a032ae486afe4c6126eb301ec3dcb1
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-02-19 12:52:01 -0600

    Serialize group id for Groupuser

--- app/serializers/group_user_serializer.rb
+++ app/serializers/group_user_serializer.rb
@@ -1,4 +1,4 @@
 class GroupUserSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :updated_at
+  attributes :id, :created_at, :updated_at, :group_id
   has_one :user, serializer: UserSerializer
 end
