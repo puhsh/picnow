164a7b6ad2d7eea57d6e7cbb0ddc999bd58fdf30
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-02-19 12:25:50 -0600

    Comments

--- spec/controllers/v1/group_users_controller_spec.rb
+++ spec/controllers/v1/group_users_controller_spec.rb
@@ -18,13 +18,13 @@ describe V1::GroupUsersController do
       expect(assigns[:group_users]).to_not be_empty
     end
 
-    it 'adds each user to the group' do
+    it 'adds multiple users to the group based on an array of user ids' do
       sign_in user
       post :create, { user_id: user.id, group_id: group.id, user_ids: [user2.id] }
       expect(assigns[:group_users].map(&:user)).to include(user2)
     end
 
-    it 'adds a single user to the group' do
+    it 'adds a single user to the group based on a single group user param' do
       sign_in user
       post :create, { user_id: user.id, group_id: group.id, group_user: { user_id: user2.id } }
       expect(assigns[:group_users].map(&:user)).to include(user2)
