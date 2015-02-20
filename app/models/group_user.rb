de4132e592a1d8e9a4f45c03849d1b89d37cbb23
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-02-19 22:00:38 -0600

    Notify people when someone joins the group

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -8,6 +8,7 @@ class GroupUser < ActiveRecord::Base
 
   # Callbacks
   after_commit :notify_user, on: :create
+  # after_commit :notify_members, on: :create
   after_destroy :record_event
 
   # Validations
@@ -26,8 +27,21 @@ class GroupUser < ActiveRecord::Base
   def notify_user
     group = self.group
     admin = self.group.admin
-    if group && admin
+    if group && admin && self.user_id != self.admin.id
       self.user.devices.each { |x| x.fire_notification!("#{admin.username} added you to the group #{group.name}", :added_to_group, { group_id: group.id, group: group.as_json }) }
     end
   end
+
+  # TODO Enable this once 1.2 goes live
+  # Protected: Notifies all the group members that someone joined the group_id
+  #
+  # Returns nothing
+  # def notify_members
+  #   group = self.group
+  #   user = self.user
+  #   group.users.each do |user|
+  #     next if user.id == self.user_id
+  #     self.user.devices.each { |x| x.fire_notification!("#{user.username} joined the group #{group.name}", :joined_group, { group_id: group.id, group: group.as_json }) }
+  #   end
+  # end
 end
